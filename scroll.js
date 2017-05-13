a04976443afd234c5908fefa12fa1550488f861d
Kevin Nguyen <knguy47@gmail.com>
2017-05-13 13:20:32 -0500

    Feature Added: Highlight All, UnHighlight, Scroll

---
+++ scroll.js
@@ -0,0 +1,48 @@
+console.log($('span.highlight').first());
+console.log($('span.highlight').parent());
+    $('body').animate({
+          scrollTop: $('span.highlight').offset().top     
+        }, 1000);
+
+// jQuery(function($) {
+  
+//   var $sections = $('.section'),
+//     $animContainer = $sections.parent(),
+//     $document = $(document),
+//     numSections = $sections.length,
+//     currSection = 0,
+//     isAnimating = false;
+  
+//   // Animate to a specific index.
+//   var gotoSection = function(index) {
+//     isAnimating = true;
+//     $animContainer.animate({
+//       scrollTop: $sections.eq(index).offset().top
+//     }, 750, function () {
+//       isAnimating = false;
+//     });
+//   };
+  
+//   // Advance to next or previous section.
+//   var handleAction = function(direction) {
+//     if (isAnimating) { return false; }
+    
+//     if (direction === 'prev' && currSection > 0) { currSection--; }
+//     else if (direction === 'next' && currSection < numSections - 1) { currSection++; }
+//     else { return false; } // This shouldn't happen.
+    
+//     gotoSection(currSection);
+//   };
+  
+//   // Handle clicks.
+//   $document.on('click', '.action', function() {
+//     handleAction($(this).data('direction'));
+//   });
+  
+//   // Handle keyboard input.
+//   $document.keyup(function(e){
+//     if (e.keyCode === 38) { handleAction('prev'); } // Up arrow.
+//     if (e.keyCode === 40) { handleAction('next'); } // Down arrow.
+//   });
+  
+// });
