464dd195cf5ebd6098bdffccdbcfa7bb658d9da9
Brian Leung <brianbrianleungleung@gmail.com>
2017-05-18 14:13:33 -0500

    Change environment variables and http endpoints for development
    Modify Auth0 domain and clientID for development functionality.
    Add env. variable for URL endpoint for development functionality.
    
    Closes #8

--- env.js
+++ env.js
@@ -1,4 +1,5 @@
 window.env = {
-  AUTH0_DOMAIN: 'dhsiao89.auth0.com',
-  AUTH0_CLIENT_ID: '7ahU6Olf4SuRFf3B3lDGVuY6DGP0hj5T',
+  AUTH0_DOMAIN: 'xosk.auth0.com',
+  AUTH0_CLIENT_ID: 'gLvvvwQlgFMIhedyBZDIjsGrb1Oa47oZ',
+  URL: 'http://localhost:3003/api/users/'
 };
