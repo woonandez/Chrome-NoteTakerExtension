a56fd2d742b3bf2492d2802289d31c72d326bd0c
Brian Leung <brianbrianleungleung@gmail.com>
2017-05-22 15:48:13 -0500

    Change highlighting to be more fluid

--- src/highlightInit.js
+++ src/highlightInit.js
@@ -1,6 +1,6 @@
 var AUTH0_DOMAIN = 'xosk.auth0.com';
 var AUTH0_CLIENT_ID = 'gLvvvwQlgFMIhedyBZDIjsGrb1Oa47oZ';
-var URL = 'http://127.0.0.1:3003/api/users/';
+var URL = 'https://study-buddy-.herokuapp.com/api/users/';
 
 function findAnnotations(container, userID, tabUrl, content) {
   $.ajax({
