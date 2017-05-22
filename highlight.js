06184ca1efa760fc2e788c29ad5d7d33018a0f09
Brian Leung <brianbrianleungleung@gmail.com>
2017-05-22 12:38:42 -0500

    Implement all-notes highlighting and annotations
    Add functionality for all-notes highlighting upon arriving to site.
    Create annotations feature:
      - Render annotation box when highlighted text is clicked
      - Allow rendering of all annotations when box is opned first
      - Create functionality for a submission button of annotations

--- highlight.js
+++ highlight.js
@@ -1,5 +1,7 @@
 chrome.storage.local.get('changes', function(results) {
   var changes = results.changes;
+  var userID = results.userID;
+  var tab = results.currentUri;
 
   changes.textToUnhighlight.forEach((value) => {
     $('body').unhighlight(value);
@@ -13,14 +15,59 @@ chrome.storage.local.get('changes', function(results) {
   $('body').append(`<h1 class="annotate">Test</h1>`);
 
   $('.highlightAnnotations').on('click', function() {
-    // $(this).css("background-color", "green");
-  });
+    if ( $(this).hasClass("highlightAnnotations") ) {
+      var span = $(this);
+      $(this).removeClass('highlightAnnotations');
+      $(this).addClass('annotate');
+      var elementPositionTop = $(this).offset().top;
+      var elementPositionLeft = $(this).offset().left;
 
-  $('.annotate').on('click', function() {
-    chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-      console.log(response.farewell);
-    });
+      var annotationDiv = $('<div class="annotation"><form><textarea row="1" col="20"></textarea><input type="submit" value="Submit"><button class="exit">Exit</button></form><br></div>')
+
+      annotationDiv.css({
+        'top': elementPositionTop,
+        'left': elementPositionLeft
+      })
+
+      // Add annotations to annotation container
+
+      $(this).prepend(annotationDiv);
+      $(this).find('form').on('submit', function(event) {
+        event.preventDefault();
+
+        // Send annotation to database
+        var annotation = postAnnotation(($(this).find('textarea').val()), userID, tab);
+        $(this).after(annotation);
+      });
+
+      $('.exit').on('click', function() {
+        $(this).parent().parent().remove();
+        span.removeClass('annotate');
+        setTimeout(function() { span.addClass('highlightAnnotations'); }, 0);
+      });
+    }
   });
 
   chrome.storage.local.remove('changes');
 });
+
+function postAnnotation(annotation, userID, url) {
+  // Ajax call to annotations
+  var annotationData = { 'name': userID, 'url': url, 'annotation': JSON.stringify(annotation) };
+
+  $.ajax({
+    url: `${URL}annotations`,
+    contentType: 'application/json',
+    type: 'POST',
+    data: annotationData,
+    success: (data) => {
+      console.log('Received data!')
+    },
+    error: (data) => {
+      console.log('Did not receive:', data)
+    }
+  });
+
+
+  return $('<div>').text(annotation);
+}
--- src/highlightInit.js
+++ src/highlightInit.js
@@ -2,6 +2,51 @@ var AUTH0_DOMAIN = 'xosk.auth0.com';
 var AUTH0_CLIENT_ID = 'gLvvvwQlgFMIhedyBZDIjsGrb1Oa47oZ';
 var URL = 'http://localhost:3003/api/users/';
 
+function findAnnotations(container, userID, url) {
+  $.ajax({
+    url: `${URL}${userID}`,
+    type: 'GET',
+    success: (data) => {
+
+      if(data.length !== 0) {
+        data[0].urls.forEach(function(url) {
+          if(url.name === tab.url) {
+            url.pins.forEach(function(note, index) {
+              $dropdown.append($("<option/>", {
+                label: `Pin ${index + 1}: ${note.slice(0, 15)}...`,
+                value: index,
+                text: note
+              }));
+            });
+          }
+        });
+      }
+
+    }
+  });
+}
+
+function postAnnotation(annotation, userID, url) {
+  // Ajax call to annotations
+  var annotationData = { 'name': userID, 'url': url, 'annotation': JSON.stringify(annotation) };
+
+  $.ajax({
+    url: `${URL}annotations`,
+    contentType: 'application/json',
+    type: 'POST',
+    data: annotationData,
+    success: (data) => {
+      console.log('Received data!')
+    },
+    error: (data) => {
+      console.log('Did not receive:', data)
+    }
+  });
+
+
+  return $('<div>').text(annotation);
+}
+
 chrome.runtime.sendMessage({init: "init"}, function(response) {
   var tab = response.url;
   var authResult = response.authResult;
@@ -30,6 +75,41 @@ chrome.runtime.sendMessage({init: "init"}, function(response) {
             }
           });
         }
+
+        // Open Annotation Box
+        $('.highlightAnnotations').on('click', function() {
+          if ( $(this).hasClass("highlightAnnotations") ) {
+            var span = $(this);
+            $(this).removeClass('highlightAnnotations');
+            $(this).addClass('annotate');
+            var elementPositionTop = $(this).offset().top;
+            var elementPositionLeft = $(this).offset().left;
+
+            var annotationDiv = $('<div class="annotation"><form><textarea row="1" col="20"></textarea><input type="submit" value="Submit"><button class="exit">Exit</button></form><br></div>')
+            annotationDiv.css({
+              'top': elementPositionTop,
+              'left': elementPositionLeft
+            })
+            $(this).prepend(annotationDiv);
+
+            // Add annotations to annotation container
+            findAnnotations(($(this).find('.annotation')), userID, tab);
+
+            // Create Annotations
+            $(this).find('form').on('submit', function(event) {
+              event.preventDefault();
+              // Send annotation to database
+              var annotation = postAnnotation(($(this).find('textarea').val()), userID, tab);
+              $(this).after(annotation);
+            });
+
+            $('.exit').on('click', function() {
+              $(this).parent().parent().remove();
+              span.removeClass('annotate');
+              setTimeout(function() { span.addClass('highlightAnnotations'); }, 0);
+            });
+          }
+        });
       },
       error: (data) => {
         console.log('Did not receive:' + data);
@@ -37,4 +117,3 @@ chrome.runtime.sendMessage({init: "init"}, function(response) {
     });
   });
 });
-
